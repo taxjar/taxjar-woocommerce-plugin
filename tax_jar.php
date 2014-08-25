07d16f1f93b317bd94d3e62e668a71b56c675f1f
tonkapark <tonkapark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 15:18:27 +0000

    tag version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/taxjar-simplified-taxes-for-woocommerce/tags/1.0.1@972466 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tax_jar.php
+++ tax_jar.php
@@ -1,13 +1,13 @@
 <?php
-/*
-Plugin Name: TaxJar - Sales Tax Calculation for WooCommerce
-Plugin URI: http://www.taxjar.com/woocommerce-sales-tax-plugin/
-Description: TaxJar for WooCommerce helps you collect <strong>accurate sales tax</strong> with almost no work! Stop uploading and updating rate tables. To get started: 1) <a href="http://www.taxjar.com/api/">Sign up for a TaxJar API token</a>, and 2) Go to your <a href="admin.php?page=wc-settings&tab=integration">TaxJar settings page</a>, and save your API token and business address.
-Version: 1.0.0
-Author: TaxJar
-Author URI: http://www.taxjar.com/
-
-*/
+/**
+ * Plugin Name: TaxJar - Sales Tax Automation for WooCommerce
+ * Plugin URI: http://taxjar.com/woocommerce
+ * Description: TaxJar for WooCommerce helps you collect <strong>accurate sales tax</strong> with almost no work! Stop uploading and updating rate tables. To get started: 1) <a href="http://www.taxjar.com/api/">Sign up for a TaxJar API token</a>, and 2) Go to your <a href="admin.php?page=wc-settings&tab=integration">TaxJar settings page</a>, and save your API token and business address.
+ * Author: TaxJar
+ * Author URI: http://taxjar.com
+ * Version: 1.0.1
+ *
+ */
 
 if ( ! class_exists( 'WC_Taxjar' ) ) :
 
@@ -29,7 +29,7 @@ class WC_Taxjar {
     if ( class_exists( 'WC_Integration' ) ) {
       // Include our integration class.
       include_once 'includes/class-wc-taxjar-integration.php';
-			
+
       // Register the integration.
       add_filter( 'woocommerce_integrations', array( $this, 'add_integration' ) );
     } else {
@@ -46,6 +46,17 @@ class WC_Taxjar {
   }
 
 }
+
+/**
+ * Adds settings link to the plugins page
+ */
+function plugin_settings_link($links) { 
+ 	$settings_link = '<a href="admin.php?page=wc-settings&tab=integration">Settings</a>'; 
+  	array_unshift($links, $settings_link); 
+  	return $links; 
+}
+
+add_filter('plugin_action_links_'. plugin_basename(__FILE__), 'plugin_settings_link');
 
 $WC_Taxjar = new WC_Taxjar( __FILE__ );
 
