fae0809641e33b47cdf3c24f31757f0669ee686b
Alec Carper <xarious@gmail.com>
2015-12-29 15:47:57 -0700

    fix shipping tax calculation + specs

--- tests/specs/test-actions.php
+++ tests/specs/test-actions.php
@@ -3,12 +3,27 @@ class TJ_WC_Actions extends WP_UnitTestCase {
   function test_use_taxjar_total() {
     global $woocommerce;
     TaxJar_Woocommerce_helper::prepare_woocommerce();
+
     $tj = new WC_Taxjar_Integration();
 
-    $this->assertTrue($woocommerce->cart->tax_total == 0);
+    $this->assertTrue($woocommerce->cart->get_taxes_total() == 0);
 
-    do_action('woocommerce_calculate_totals', $woocommerce->cart);    
+    do_action('woocommerce_calculate_totals', $woocommerce->cart);
 
-    $this->assertTrue($woocommerce->cart->tax_total != 0);
+    $this->assertTrue($woocommerce->cart->get_taxes_total() != 0);
+  }
+
+  function test_the_correct_taxes_are_set() {
+    global $woocommerce;
+    TaxJar_Woocommerce_helper::prepare_woocommerce();
+    $tj = new WC_Taxjar_Integration();
+
+    $woocommerce->shipping->shipping_total = 5;
+
+    do_action('woocommerce_calculate_totals', $woocommerce->cart);
+
+    $this->assertEquals($woocommerce->cart->tax_total, 0.73, '', 0.001);
+    $this->assertEquals(array_values($woocommerce->cart->shipping_taxes)[0], 0.36, '', 0.001);
+    $this->assertEquals($woocommerce->cart->get_taxes_total(), 1.09, '', 0.001);
   }
 }
--- tests/specs/test-filters.php
+++ tests/specs/test-filters.php
@@ -7,13 +7,14 @@ class TJ_WC_Filters extends WP_UnitTestCase {
   function test_append_base_address_to_customer_taxable_address() {
     global $woocommerce;
     TaxJar_Woocommerce_helper::prepare_woocommerce();
+
     $tj = new WC_Taxjar_Integration();
     $woocommerce->session->set('chosen_shipping_methods', array('local_pickup'));
 
     $address = array('US', 'CO', '81210', 'Denver');
     $address = apply_filters('woocommerce_customer_taxable_address', $address);
 
-    $this->assertTrue($address[2] == $tj->settings['store_zip']);
-    $this->assertTrue($address[3] == $tj->settings['store_city']);
+    $this->assertEquals(strtoupper($address[2]), strtoupper($tj->settings['store_zip']));
+    $this->assertEquals(strtoupper($address[3]), strtoupper($tj->settings['store_city']));
   }
 }
