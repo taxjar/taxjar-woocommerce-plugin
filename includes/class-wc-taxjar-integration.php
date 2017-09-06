637b36d2744a11b90eb6370c578ca747ba246122
Jake Johnson <jake@fastdivision.com>
2017-09-06 11:48:49 -0700

    Improve tax rate override notice

--- includes/class-wc-taxjar-integration.php
+++ includes/class-wc-taxjar-integration.php
@@ -848,8 +848,8 @@ class WC_Taxjar_Integration extends WC_Integration {
 	 *
 	 */
 	public function output_sections_before() {
-		echo '<div class="taxjar"><h3>Tax Rates Powered by <a href="http://www.taxjar.com" target="_blank">TaxJar</a>. <a href="admin.php?page=wc-settings&tab=integration">Configure TaxJar</a></h3></div>';
-		echo '<div style="display:none;">';
+		echo '<div class="updated taxjar-notice"><p><b>Powered by <a href="https://www.taxjar.com" target="_blank">TaxJar</a></b> ― Your tax rates and settings are automatically configured.</p><p><a href="admin.php?page=wc-settings&tab=integration" class="button-primary">Configure TaxJar</a> &nbsp; <a href="https://www.taxjar.com/contact/" class="button" target="_blank">Help &amp; Support</a></p></div>';
+		echo '<div style="display: none">';
 	}
 
 	/**
