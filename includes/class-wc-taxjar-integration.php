138fb95bced672852057e81dfecf4b4b6b9d7278
Jake Johnson <jake@fastdivision.com>
2017-12-11 14:39:23 -0800

    Exempt line items with "Zero rate" tax class applied

--- includes/class-wc-taxjar-integration.php
+++ includes/class-wc-taxjar-integration.php
@@ -509,7 +509,7 @@ class WC_Taxjar_Integration extends WC_Integration {
 			$tax_class = explode( '-', $product->get_tax_class() );
 			$tax_code = '';
 
-			if ( ! $product->is_taxable() ) {
+			if ( ! $product->is_taxable() || 'zero-rate' == $product->get_tax_class() ) {
 				$tax_code = '99999';
 			}
 
@@ -618,7 +618,7 @@ class WC_Taxjar_Integration extends WC_Integration {
 			$unit_price = $product->get_price();
 			$tax_code = '';
 
-			if ( ! $product->is_taxable() ) {
+			if ( ! $product->is_taxable() || 'zero-rate' == $product->get_tax_class() ) {
 				$tax_code = '99999';
 			}
 
