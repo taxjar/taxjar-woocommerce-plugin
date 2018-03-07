d85913bac2ea2804e208b438ae41bc9456f29ec4
Jake Johnson <jake@fastdivision.com>
2018-03-06 19:26:38 -0800

    Update "tested up to" for WooCommerce 3.3.x

--- taxjar-woocommerce.php
+++ taxjar-woocommerce.php
@@ -7,7 +7,7 @@
  * Author: TaxJar
  * Author URI: https://www.taxjar.com
  * WC requires at least: 2.6.0
- * WC tested up to: 3.2.0
+ * WC tested up to: 3.3.0
  *
  * Copyright: © 2014-2017 TaxJar. TaxJar is a trademark of TPS Unlimited, Inc.
  * License: GNU General Public License v2.0 or later
