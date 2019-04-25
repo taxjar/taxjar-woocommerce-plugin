21f3c049514be21f9f1d51675e14e1da9f4d3c8c
Dallen Dalton <dallendalton@gmail.com>
2019-04-25 18:42:23 +0000

    Confirm compatibility with WooCommerce 3.6.2

--- taxjar-woocommerce.php
+++ taxjar-woocommerce.php
@@ -7,7 +7,7 @@
  * Author: TaxJar
  * Author URI: https://www.taxjar.com
  * WC requires at least: 2.6.0
- * WC tested up to: 3.5.0
+ * WC tested up to: 3.6.2
  *
  * Copyright: © 2014-2019 TaxJar. TaxJar is a trademark of TPS Unlimited, Inc.
  * License: GNU General Public License v2.0 or later
