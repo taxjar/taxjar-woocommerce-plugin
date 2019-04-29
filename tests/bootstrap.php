3d788a28074f0d05acbc60fba29549f62c6f4876
Dallen Dalton <dallendalton@gmail.com>
2019-04-29 23:18:04 +0000

    Remove setting WooCommerce constants that cause issues with WooCommerce versions older than 3.6.0

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -35,8 +35,6 @@ class TaxJar_WC_Unit_Tests_Bootstrap {
 	}
 
 	public function load_wc() {
-		define( 'WC_TAX_ROUNDING_MODE', 'auto' );
-		define( 'WC_USE_TRANSACTIONS', false );
 		require_once $this->plugin_dir . 'woocommerce/woocommerce.php';
 
 		// load taxjar core
