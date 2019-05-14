15b5768370d11cc6dba79d609a54c9ffc1616823
Dallen Dalton <dallendalton@gmail.com>
2019-05-14 19:53:25 +0000

    Hook and method for processing batches

--- includes/class-wc-taxjar-transaction-sync.php
+++ includes/class-wc-taxjar-transaction-sync.php
@@ -12,6 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WC_Taxjar_Transaction_Sync {
 
 	const PROCESS_QUEUE_HOOK = 'taxjar_process_queue';
+	const PROCESS_BATCH_HOOK = 'taxjar_process_record_batch';
 	const QUEUE_GROUP = 'taxjar-queue-group';
 
 	public function __construct( $integration ) {
@@ -21,6 +22,7 @@ class WC_Taxjar_Transaction_Sync {
 	public static function init() {
 		add_action( 'init', array( __CLASS__, 'schedule_process_queue' ) );
 		add_action( self::PROCESS_QUEUE_HOOK, array( __CLASS__, 'process_queue' ) );
+		add_action( self::PROCESS_BATCH_HOOK, array( __CLASS__, 'process_batch' ) );
 	}
 
 	public static function schedule_process_queue() {
@@ -51,9 +53,18 @@ class WC_Taxjar_Transaction_Sync {
 		$batches = array_chunk( $active_records, apply_filters( 'taxjar_record_batch_size', 50 ) );
 
 		foreach( $batches as $batch ) {
-			$batch_id = as_schedule_single_action( time(), 'taxjar_process_record_batch', array( 'queue_ids' => $batch ), self::QUEUE_GROUP );
+			$batch_id = as_schedule_single_action( time(), self::PROCESS_BATCH_HOOK, array( 'queue_ids' => $batch ), self::QUEUE_GROUP );
 			WC_Taxjar_Record_Queue::add_records_to_batch( $batch, $batch_id );
 		}
+	}
+
+	/**
+	 * Process the batch and sync records to TaxJar
+	 *
+	 * @return null
+	 */
+	public static function process_batch( $args ) {
+		
 	}
 
 }
