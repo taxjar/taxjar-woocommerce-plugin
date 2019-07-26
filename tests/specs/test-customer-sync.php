013386d1765832aeeef9d83eaa8e01ee32140223
Dallen Dalton <dallendalton@gmail.com>
2019-07-26 22:06:05 +0000

    Spec testing customer data validation before sync

--- tests/specs/test-customer-sync.php
+++ tests/specs/test-customer-sync.php
@@ -13,4 +13,30 @@ class TJ_WC_Test_Customer_Sync extends WP_UnitTestCase {
 		WC_Taxjar_Record_Queue::clear_queue();
 	}
 
+	function test_customer_sync_validation() {
+		$customer = TaxJar_Customer_Helper::create_customer();
+		$customer->set_email( 'test@test.com' );
+		$customer->save();
+
+		// test no object loaded
+		$record = new TaxJar_Customer_Record( $customer->get_id(), true );
+		$should_sync = $record->should_sync();
+		$this->assertFalse( $should_sync );
+
+		// test no name
+		$record = new TaxJar_Customer_Record( $customer->get_id(), true );
+		$record->load_object();
+		$should_sync = $record->should_sync();
+		$this->assertFalse( $should_sync );
+
+		$customer->set_billing_first_name( 'Test' );
+		$customer->set_billing_last_name( 'Test' );
+		$customer->save();
+
+		$record = new TaxJar_Customer_Record( $customer->get_id(), true );
+		$record->load_object();
+		$should_sync = $record->should_sync();
+		$this->assertTrue( $should_sync );
+	}
+
 }
