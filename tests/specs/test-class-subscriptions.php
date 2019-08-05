c39713befdfb83ae9936a00942625e4163ff87c7
Dallen Dalton <dallendalton@gmail.com>
2019-08-05 22:20:27 +0000

    Remove expected depricated annotations to prevent spec failure on older woocommerce versions

--- tests/specs/test-class-subscriptions.php
+++ tests/specs/test-class-subscriptions.php
@@ -369,9 +369,6 @@ class TJ_WC_Class_Subscriptions extends WP_HTTP_TestCase {
 		}
 	}
 
-	/**
-	 * @expectedDeprecated get_used_coupons
-	 */
 	function test_correct_taxes_for_subscription_recurring_order() {
 		wp_set_current_user( $this->user );
 
@@ -401,9 +398,6 @@ class TJ_WC_Class_Subscriptions extends WP_HTTP_TestCase {
 		TaxJar_Shipping_Helper::delete_simple_flat_rate();
 	}
 
-	/**
-	 * @expectedDeprecated get_used_coupons
-	 */
 	function test_correct_taxes_for_subscription_recurring_order_with_one_month_trial() {
 		wp_set_current_user( $this->user );
 
@@ -444,9 +438,6 @@ class TJ_WC_Class_Subscriptions extends WP_HTTP_TestCase {
 		TaxJar_Shipping_Helper::delete_simple_flat_rate();
 	}
 
-	/**
-	 * @expectedDeprecated get_used_coupons
-	 */
 	function test_correct_taxes_for_subscription_recurring_order_with_trial_and_signup_fee() {
 		wp_set_current_user( $this->user );
 
@@ -486,10 +477,7 @@ class TJ_WC_Class_Subscriptions extends WP_HTTP_TestCase {
 
 		TaxJar_Shipping_Helper::delete_simple_flat_rate();
 	}
-
-	/**
-	 * @expectedDeprecated get_used_coupons
-	 */
+	
 	function test_correct_taxes_for_subscription_recurring_order_with_multiple_products() {
 		wp_set_current_user( $this->user );
 
