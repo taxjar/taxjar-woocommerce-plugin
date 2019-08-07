4aa99fae0fa888da6538facc6d913c2ab0ed47fa
Dallen Dalton <dallendalton@gmail.com>
2019-08-07 15:55:26 +0000

    Clean up state with space fix

--- includes/class-wc-taxjar-integration.php
+++ includes/class-wc-taxjar-integration.php
@@ -459,25 +459,19 @@ class WC_Taxjar_Integration extends WC_Integration {
 			'tax_rate_class' => $tax_class,
 		);
 
-		if ( version_compare( WC()->version, '3.2.0', '<' ) ) {
-			$wc_rate = WC_Tax::find_rates( array(
-				'country' => $location['to_country'],
-				'state' => $location['to_state'],
-				'postcode' => $location['to_zip'],
-				'city' => $location['to_city'],
-				'tax_class' => $tax_class,
-			) );
-        } else {
-			$wc_rate = WC_Tax::find_rates( array(
-				'country' => $location['to_country'],
-				'state' => sanitize_key( $location['to_state'] ),
-				'postcode' => $location['to_zip'],
-				'city' => $location['to_city'],
-				'tax_class' => $tax_class,
-			) );
-        }
+		$rate_lookup = array(
+			'country' => $location['to_country'],
+			'state' => $location['to_state'],
+			'postcode' => $location['to_zip'],
+			'city' => $location['to_city'],
+			'tax_class' => $tax_class,
+		);
 
-
+		if ( version_compare( WC()->version, '3.2.0', '>=' ) ) {
+			$rate_lookup['state'] = sanitize_key( $location['to_state'] );
+		}
+		
+		$wc_rate = WC_Tax::find_rates( $rate_lookup );
 		if ( ! empty( $wc_rate ) ) {
 			$this->_log( ':: Tax Rate Found ::' );
 			$this->_log( $wc_rate );
