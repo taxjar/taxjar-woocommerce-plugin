6b97907c95fe5da1ac616cedfe242cfea57ebf9a
Dallen Dalton <dallendalton@gmail.com>
2020-05-19 21:01:28 +0000

    Add tests for shipping and exempt products for API orders

--- tests/framework/tj-wc-rest-unit-test-case.php
+++ tests/framework/tj-wc-rest-unit-test-case.php
@@ -60,7 +60,7 @@ class TJ_WC_REST_Unit_Test_Case extends WP_HTTP_TestCase {
 	/**
 	 * Tests tax calculation on a simple order created through the WooCommerce REST API
 	 */
-	function test_simple_product_api_order_calculation() {
+	function test_simple_product_tax_on_api_order() {
 		$product_id = TaxJar_Product_Helper::create_product( 'simple' )->get_id();
 
 		$request = new WP_REST_Request( 'POST', $this->create_order_endpoint );
@@ -85,6 +85,77 @@ class TJ_WC_REST_Unit_Test_Case extends WP_HTTP_TestCase {
 
 		foreach( $order->get_items() as $item ) {
 			$this->assertEquals( 0.73, $item->get_total_tax() );
+		}
+	}
+
+	function test_shipping_tax_on_api_order() {
+		$product_id = TaxJar_Product_Helper::create_product( 'simple' )->get_id();
+
+		$request = new WP_REST_Request( 'POST', $this->create_order_endpoint );
+		$request_body = TaxJar_API_Order_Helper::create_order_request_body(
+			array(
+				'shipping'             => array(
+					'first_name' => 'Test',
+					'last_name'  => 'Customer',
+					'address_1'  => '123 Main St.',
+					'address_2'  => '',
+					'city'       => 'Greenwood Village',
+					'state'      => 'CO',
+					'postcode'   => '80111',
+					'country'    => 'US',
+				),
+				'line_items'           => array(
+					array(
+						'product_id' => $product_id,
+						'quantity'   => 1
+					)
+				)
+			)
+		);
+		$request->set_body_params( $request_body );
+
+		$response = $this->server->dispatch( $request );
+		$data     = $response->get_data();
+		$order    = wc_get_order( $data['id'] );
+
+		$this->assertEquals( 201, $response->get_status() );
+		$this->assertEquals( 1.46, $order->get_total_tax() );
+		$this->assertEquals( 0.73, $order->get_shipping_tax() );
+
+		foreach( $order->get_items() as $item ) {
+			$this->assertEquals( 0.73, $item->get_total_tax() );
+		}
+	}
+
+	function test_product_exemption_on_api_order() {
+		$exempt_product_id = TaxJar_Product_Helper::create_product( 'simple', array(
+			'price' => '10',
+			'sku' => 'EXEMPT-GIFT-CARD',
+			'tax_class' => 'gift-card-14111803A0001',
+		) )->get_id();
+
+		$request = new WP_REST_Request( 'POST', $this->create_order_endpoint );
+		$request_body = TaxJar_API_Order_Helper::create_order_request_body(
+			array(
+				'line_items'           => array(
+					array(
+						'product_id' => $exempt_product_id,
+						'quantity'   => 1
+					)
+				)
+			)
+		);
+		$request->set_body_params( $request_body );
+
+		$response = $this->server->dispatch( $request );
+		$data     = $response->get_data();
+		$order    = wc_get_order( $data['id'] );
+
+		$this->assertEquals( 201, $response->get_status() );
+		$this->assertEquals( 0.00, $order->get_total_tax() );
+
+		foreach( $order->get_items() as $item ) {
+			$this->assertEquals( 0.00, $item->get_total_tax() );
 		}
 	}
 
