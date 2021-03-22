1ae9d0173cfc7eb969fce113767081d41b129954
Dallen Dalton <dallendalton@gmail.com>
2021-03-22 16:42:24 -0600

    Update test product helper to correctly create subscription product

--- tests/framework/product-helper.php
+++ tests/framework/product-helper.php
@@ -38,12 +38,15 @@ class TaxJar_Product_Helper {
 
 	private static function create_subscription_product( $opts = array() ) {
 		$defaults = array(
-			'price' => '19.99',
-			'sku' => 'SUBSCRIPTION1',
-			'tax_class' => '',
-			'tax_status' => 'taxable',
-			'downloadable' => 'no',
-			'virtual' => 'yes',
+			'name'          => 'Dummy Product',
+			'price'         => '19.99',
+			'sku'           => 'SUBSCRIPTION1',
+			'manage_stock'  => false,
+			'tax_status'    => 'taxable',
+			'downloadable'  => false,
+			'virtual'       => false,
+			'stock_status'  => 'instock',
+			'weight'        => '1.1',
 			'interval' => 1,
 			'period' => 'month',
 			'sign_up_fee' => 0,
@@ -51,43 +54,22 @@ class TaxJar_Product_Helper {
 			'trial_period' => 'month',
 		);
 
-		$post = array(
-			'post_title' => 'Dummy Subscription',
-			'post_type' => 'product',
-			'post_status' => 'publish',
-		);
-		$post_meta = array_replace_recursive( $defaults, $opts );
-		$post_meta['regular_price'] = $post_meta['price'];
-
-		$post_id = wp_insert_post( $post );
-
-		register_taxonomy(
-			'product_type',
-			'subscription'
-		);
-
-		update_post_meta( $post_id, '_price', $post_meta['price'] );
-		update_post_meta( $post_id, '_regular_price', $post_meta['regular_price'] );
-		update_post_meta( $post_id, '_sale_price', '' );
-		update_post_meta( $post_id, '_sku', $post_meta['sku'] );
-		update_post_meta( $post_id, '_manage_stock', 'no' );
-		update_post_meta( $post_id, '_tax_class', $post_meta['tax_class'] );
-		update_post_meta( $post_id, '_tax_status', $post_meta['tax_status'] );
-		update_post_meta( $post_id, '_downloadable', $post_meta['downloadable'] );
-		update_post_meta( $post_id, '_virtual', $post_meta['virtual'] );
-		update_post_meta( $post_id, '_stock_status', 'instock' );
+		$props = array_replace_recursive( $defaults, $opts );
+		$props[ 'regular_price' ] = $props[ 'price' ];
+		$product = new WC_Product_Subscription();
+		$product->set_props( $props );
+		$product->save();
+		$product_id = $product->get_id();
 
 		// Subscription meta
-		update_post_meta( $post_id, '_subscription_price', $post_meta['price'] );
-		update_post_meta( $post_id, '_subscription_period_interval', $post_meta['interval'] );
-		update_post_meta( $post_id, '_subscription_period', $post_meta['period'] );
-		update_post_meta( $post_id, '_subscription_sign_up_fee', $post_meta['sign_up_fee'] );
-		update_post_meta( $post_id, '_subscription_trial_length', $post_meta['trial_length'] );
-		update_post_meta( $post_id, '_subscription_trial_period', $post_meta['trial_period'] );
+		update_post_meta( $product_id, '_subscription_price', $props['price'] );
+		update_post_meta( $product_id, '_subscription_period_interval', $props['interval'] );
+		update_post_meta( $product_id, '_subscription_period', $props['period'] );
+		update_post_meta( $product_id, '_subscription_sign_up_fee', $props['sign_up_fee'] );
+		update_post_meta( $product_id, '_subscription_trial_length', $props['trial_length'] );
+		update_post_meta( $product_id, '_subscription_trial_period', $props['trial_period'] );
 
-		wp_set_object_terms( $post_id, 'subscription', 'product_type' );
-
-		return new WC_Product_Subscription( $post_id );
+		return new WC_Product_Subscription( $product_id );
 	}
 
 }
