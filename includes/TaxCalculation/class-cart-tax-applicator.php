5f0f67ab6be63bf173a9212eab7ef018b6d910a3
Dharmesh Patel <dspatel44@gmail.com>
2022-02-15 04:06:37 +0530

    enhancement/215: Added action after cart calculate totals (#216)
    Add action after cart calculate totals

--- includes/TaxCalculation/class-cart-tax-applicator.php
+++ includes/TaxCalculation/class-cart-tax-applicator.php
@@ -279,6 +279,15 @@ class Cart_Tax_Applicator extends Tax_Applicator {
 
 		$this->cart->set_total_tax( $tax_total );
 		$this->cart->set_total( max( 0, $total ) );
+
+		/**
+		 * Fires once the cart totals calculation are done.
+		 *
+		 * @since 4.1.1
+		 *
+		 * @param WC_Cart $this->cart Cart.
+		 */
+		do_action( 'taxjar_after_calculate_cart_totals', $this->cart );
 	}
 
 }
